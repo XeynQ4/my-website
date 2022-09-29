5ef487784b8bbdbe70340ac4d939f7db36ec66bb
Oskar Danovsky <oskar.danovsky@gmail.com>
2022-09-29 17:46:14 +0200

    changed

--- src/pages/SerpinskiTriangle.js
+++ src/pages/SerpinskiTriangle.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ReactP5Wrapper } from 'react-p5-wrapper';
-import sketch from './p5/serpinskiRandomPoints';
+import sketch from './p5/serpinskiTriangle';
 
 function SerpinskiTriangle() {
     return <ReactP5Wrapper sketch={sketch}></ReactP5Wrapper>;
