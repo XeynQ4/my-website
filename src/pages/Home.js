8d5ed5252dff01a6230096dda69883d275c67c85
Xeyn <oskar.danovsky@gmail.com>
2024-08-30 15:11:37 +0300

    Linked to the correct file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -66,7 +66,7 @@ function Home() {
                     <div className="card-text no-margin-top">
                         <a
                             className="no-underline"
-                            href="../../public/Xeyn.key"
+                            href="/Xeyn.key"
                         >
                             <FontAwesomeIcon icon={faLock}></FontAwesomeIcon>{' '}
                             <span className="underline">PGP key</span>
